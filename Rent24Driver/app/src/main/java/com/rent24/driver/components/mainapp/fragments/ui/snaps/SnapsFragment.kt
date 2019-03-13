ba7676c9060c4f97c98c831ae8a652f4349b17b0
Ateeb Ahmed <ateeb_ahmed33@hotmail.com>
2019-03-14 02:46:58 +0500

    Add snaps fragment

---
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/mainapp/fragments/ui/snaps/SnapsFragment.kt
@@ -0,0 +1,29 @@
+package com.rent24.driver.components.mainapp.fragments.ui.snaps
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rent24.driver.R
+
+class SnapsFragment : Fragment() {
+
+    private lateinit var viewModel: SnapsViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.snaps_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this)
+            .get(SnapsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    companion object {
+        fun newInstance() = SnapsFragment()
+    }
+}
---
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/mainapp/fragments/ui/snaps/SnapsViewModel.kt
@@ -0,0 +1,7 @@
+package com.rent24.driver.components.mainapp.fragments.ui.snaps
+
+import androidx.lifecycle.ViewModel
+
+class SnapsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
