0c62fd333dbfd915541de42860573d992ac87756
Ateeb Ahmed <ateeb_ahmed33@hotmail.com>
2019-03-14 02:45:34 +0500

    Add invoice fragment

---
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/mainapp/fragments/ui/invoice/InvoiceFragment.kt
@@ -0,0 +1,31 @@
+package com.rent24.driver.components.mainapp.fragments.ui.invoice
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.rent24.driver.R
+
+class InvoiceFragment : Fragment() {
+
+    private lateinit var viewModel: InvoiceViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.invoice_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this)
+            .get(InvoiceViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    companion object {
+        fun newInstance() = InvoiceFragment()
+    }
+
+}
---
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/mainapp/fragments/ui/invoice/InvoiceViewModel.kt
@@ -0,0 +1,7 @@
+package com.rent24.driver.components.mainapp.fragments.ui.invoice
+
+import androidx.lifecycle.ViewModel;
+
+class InvoiceViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
