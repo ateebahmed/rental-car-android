42f5f3c9c7368cb402498d24c720943ad8e4bb71
Ateeb Ahmed <ateeb_ahmed33@hotmail.com>
2019-03-14 02:46:40 +0500

    Add profile fragment

---
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/mainapp/fragments/ui/profile/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package com.rent24.driver.components.mainapp.fragments.ui.profile
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.rent24.driver.R
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
+}
---
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/mainapp/fragments/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.rent24.driver.components.mainapp.fragments.ui.profile
+
+import androidx.lifecycle.ViewModel;
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
