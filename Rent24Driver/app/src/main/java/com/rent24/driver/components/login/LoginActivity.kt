4b6c084bb8e5e22127a82846c954438b35ec8998
Ateeb Ahmed <ateeb_ahmed33@hotmail.com>
2019-03-24 04:53:40 +0500

    Refactor LoginActivity for two-way databinding

--- Rent24Driver/app/src/main/java/com/rent24/driver/components/login/LoginActivity.kt
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/login/LoginActivity.kt
@@ -1,14 +1,8 @@
 package com.rent24.driver.components.login
 
-import android.Manifest.permission.READ_CONTACTS
-import android.app.Activity
-import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -17,7 +11,8 @@ import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.snackbar.Snackbar
 import com.rent24.driver.R
 import com.rent24.driver.databinding.ActivityLoginBinding
-import kotlinx.android.synthetic.main.activity_login.*
+
+private val TAG = LoginActivity::class.java.name
 
 /**
  * A login screen that offers login via email/password.
@@ -25,48 +20,6 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private val TAG = LoginActivity::class.java.name
-    private val signInButtonClickListener: View.OnClickListener by lazy {
-        View.OnClickListener {
-            if (binding.email
-                    .text
-                    .isNotEmpty() && binding.password
-                    .text.isNotEmpty()
-            ) {
-                binding.loginProgress
-                    .show()
-                binding.model?.callLoginApi(
-                    binding.email
-                        .text
-                        .toString(), binding.password
-                        .text
-                        .toString()
-                )
-            } else {
-                Snackbar.make(binding.container, "One or more inputs are empty", Snackbar.LENGTH_SHORT)
-                    .show()
-            }
-        }
-    }
-    private val onSharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener by lazy {
-        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences: SharedPreferences?, key: String? ->
-            binding.loginProgress
-                .hide()
-            key ?: return@OnSharedPreferenceChangeListener
-            when (key) {
-                "token" -> {
-                    Log.i(TAG, "login successful")
-                    setResult(Activity.RESULT_OK)
-                    finish()
-                }
-                "error" -> Snackbar.make(binding.container, sharedPreferences?.getString(key,
-                    "Error occured while processing")!!, Snackbar.LENGTH_SHORT)
-                    .show()
-                else -> Snackbar.make(binding.container, "Wrong credentials, Try again", Snackbar.LENGTH_SHORT)
-                    .show()
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,77 +30,64 @@ class LoginActivity : AppCompatActivity() {
         binding.model = ViewModelProviders.of(this, ViewModelProvider.AndroidViewModelFactory
             .getInstance(application))
             .get(LoginViewModel::class.java)
-        setupModel(binding.model!!)
+        setupModelObservers(binding.model!!)
         setupActionBar()
     }
 
     override fun onStart() {
         super.onStart()
         PreferenceManager.getDefaultSharedPreferences(applicationContext)
-            .registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener)
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            }
-        }
+            .registerOnSharedPreferenceChangeListener(binding.model
+                ?.onSharedPreferenceChangeListener)
     }
 
     override fun onBackPressed() {
-        if (binding.loginProgress
-                .isShown) {
-            binding.loginProgress
-                .hide()
-        }
-        setResult(Activity.RESULT_CANCELED)
+        binding.model
+            ?.onActivityBackPressed()
         super.onBackPressed()
     }
 
     override fun onStop() {
         PreferenceManager.getDefaultSharedPreferences(applicationContext)
-            .unregisterOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener)
+            .unregisterOnSharedPreferenceChangeListener(binding.model
+                ?.onSharedPreferenceChangeListener)
         super.onStop()
     }
 
-    private fun setupModel(model: LoginViewModel) {
-        Log.d(TAG, "Starting LoginActivity.setupModel")
-        model.getEmail()
-            .observe(this, Observer<String> { newEmail ->
-                binding.email
-                    .setText(newEmail)
-            })
+    private fun setupModelObservers(model: LoginViewModel) {
+        Log.d(TAG, "Starting LoginActivity.setupModelObservers")
 
-        model.getPassword()
-            .observe(this, Observer<String> { newPassword ->
-                binding.password
-                    .setText(newPassword)
-            })
-
+        binding.email
+            .onFocusChangeListener = model.onEmailFocusChangeListener
+        binding.password
+            .onFocusChangeListener = model.onPasswordFocusChangeListener
         binding.emailSignInButton
-            .setOnClickListener(signInButtonClickListener)
-    }
-
-    private fun mayRequestContacts(): Boolean {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(email, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                .setAction(android.R.string.ok)
-                { requestPermissions(arrayOf(READ_CONTACTS), REQUEST_READ_CONTACTS) }
-        } else {
-            requestPermissions(arrayOf(READ_CONTACTS),
-                REQUEST_READ_CONTACTS
-            )
-        }
-        return false
+            .setOnClickListener(model.signInButtonClickListener)
+        binding.model
+            ?.shouldShowLoadingProgressBar()!!
+            .observe(this, Observer {
+                if (it && !binding.loginProgress
+                        .isShown) {
+                    binding.loginProgress
+                        .show()
+                } else if (binding.loginProgress
+                        .isShown) {
+                    binding.loginProgress
+                        .hide()
+                }
+            })
+        binding.model
+            ?.getSnackbarMessage()!!
+            .observe(this, Observer {
+                Snackbar.make(binding.container, it, Snackbar.LENGTH_SHORT)
+                    .show()
+            })
+        binding.model
+            ?.getActivityResult()!!
+            .observe(this, Observer {
+                setResult(it)
+                finish()
+            })
     }
 
     /**
@@ -155,13 +95,5 @@ class LoginActivity : AppCompatActivity() {
      */
     private fun setupActionBar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    companion object {
-
-        /**
-         * Id to identity READ_CONTACTS permission request.
-         */
-        private val REQUEST_READ_CONTACTS = 0
     }
 }
--- Rent24Driver/app/src/main/java/com/rent24/driver/components/login/LoginViewModel.kt
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/login/LoginViewModel.kt
@@ -1,13 +1,21 @@
 package com.rent24.driver.components.login
 
+import android.app.Activity
 import android.app.Application
+import android.content.SharedPreferences
 import android.preference.PreferenceManager
+import android.util.Log
+import android.view.View
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.rent24.driver.api.login.request.LoginRequest
 import com.rent24.driver.api.login.response.LoginResponse
 import com.rent24.driver.repository.ApiManager
+
+private val TAG = LoginViewModel::class.java.name
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -18,16 +26,52 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         PreferenceManager.getDefaultSharedPreferences(getApplication<Application>().applicationContext)
             .getString("token", "")
     }
-
-    fun getEmail(): LiveData<String> {
-        return email
+    val signInButtonClickListener by lazy {
+        View.OnClickListener {
+            if (validateFields()) {
+                showLoadingProgressBar.value = true
+                callLoginApi(email.value ?: "", password.value ?: "")
+            } else {
+                snackbarMessage.value = "One or more inputs are empty"
+            }
+        }
     }
-
-    fun getPassword(): LiveData<String> {
-        return password
+    val onEmailFocusChangeListener by lazy {
+        View.OnFocusChangeListener { v, focus ->
+            val editText = (v as AppCompatAutoCompleteTextView)
+            if (!focus && editText.text?.isNotEmpty() == true) {
+                email.value = editText.text.toString()
+            }
+        }
     }
+    val onPasswordFocusChangeListener by lazy {
+        View.OnFocusChangeListener { v, focus ->
+            val editText = (v as AppCompatEditText)
+            if (!focus && editText.text?.isNotEmpty() == true) {
+                password.value = editText.text.toString()
+            }
+        }
+    }
+    private val showLoadingProgressBar by lazy { MutableLiveData<Boolean>() }
+    private val snackbarMessage by lazy { MutableLiveData<String>() }
+    val onSharedPreferenceChangeListener by lazy {
+        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences: SharedPreferences?, key: String? ->
+            showLoadingProgressBar.value = false
+            key ?: return@OnSharedPreferenceChangeListener
+            when (key) {
+                "token" -> {
+                    Log.i(TAG, "login successful")
+                    activityResult.value = Activity.RESULT_OK
+                }
+                "error" -> snackbarMessage.value =
+                    sharedPreferences?.getString(key, "Error occured while processing")!!
+                else -> snackbarMessage.value = "Wrong credentials, Try again"
+            }
+        }
+    }
+    private val activityResult by lazy { MutableLiveData<Int>() }
 
-    fun callLoginApi(email: String, password: String) {
+    private fun callLoginApi(email: String, password: String) {
         apiManager.login(LoginRequest(email, password), this)
     }
 
@@ -45,5 +89,28 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
             }
             apply()
         }
+    }
+
+    private fun validateFields(): Boolean {
+        return (email.value
+                ?.isNotEmpty() == true && password.value
+                ?.isNotEmpty() == true)
+    }
+
+    fun shouldShowLoadingProgressBar(): LiveData<Boolean> {
+        return showLoadingProgressBar
+    }
+
+    fun getSnackbarMessage(): LiveData<String> {
+        return snackbarMessage
+    }
+
+    fun getActivityResult(): LiveData<Int> {
+        return activityResult
+    }
+
+    fun onActivityBackPressed() {
+        showLoadingProgressBar.value = false
+        activityResult.value = Activity.RESULT_CANCELED
     }
 }
