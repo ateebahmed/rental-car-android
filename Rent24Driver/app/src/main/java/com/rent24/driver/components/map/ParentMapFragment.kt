5e55b7bf5da4aac2f35e963370ab72649f7e9357
Ateeb Ahmed <ateeb_ahmed33@hotmail.com>
2019-03-27 03:30:08 +0500

    Detect location of user and ask for permission if necessary on runtime

--- Rent24Driver/app/src/main/java/com/rent24/driver/components/map/ParentMapFragment.kt
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/map/ParentMapFragment.kt
@@ -1,6 +1,7 @@
 package com.rent24.driver.components.map
 
 import android.Manifest
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -17,10 +21,12 @@ import com.google.android.material.snackbar.Snackbar
 import com.rent24.driver.R
 import com.rent24.driver.databinding.ParentMapFragmentBinding
 
-private const val COARSE_LOCATION_REQUEST_CODE = 2
+private const val LOCATION_REQUEST_CODE = 2
 private val TAG = ParentMapFragment::class.java.name
+private const val LOCATION_SETTINGS_REQUEST_CODE = 3
+private const val FINE_LOCATION_REQUEST_CODE = 4
 
-class ParentMapFragment : Fragment(), OnMapReadyCallback {
+class ParentMapFragment : Fragment(), OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private lateinit var viewModel: ParentMapViewModel
     private lateinit var mMap: GoogleMap
@@ -36,7 +42,6 @@ class ParentMapFragment : Fragment(), OnMapReadyCallback {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ParentMapViewModel::class.java)
         setupModelObservers(viewModel)
-        getLastLocation()
         val map = SupportMapFragment()
         childFragmentManager.beginTransaction()
             .replace(R.id.frame_layout, map)
@@ -55,11 +60,25 @@ class ParentMapFragment : Fragment(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        viewModel.onMapReady()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        viewModel.onRequestPermissionsResult(requestCode, grantResults, COARSE_LOCATION_REQUEST_CODE)
+        viewModel.onRequestPermissionsResult(requestCode, grantResults, LOCATION_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d(TAG, "result code $resultCode, request code $requestCode")
+        viewModel.onActivityResult(requestCode, resultCode, LOCATION_SETTINGS_REQUEST_CODE)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.updateLastLocation()
+    }
+
+    override fun onPause() {
+        viewModel.stopLocationUpdates()
+        super.onPause()
     }
 
     private fun setupModelObservers(model: ParentMapViewModel) {
@@ -79,7 +98,7 @@ class ParentMapFragment : Fragment(), OnMapReadyCallback {
         model.askForLocationPermission()
             .observe(this, Observer {
                 if (it) {
-                    askForPermission()
+                    askForLocationPermission()
                 }
             })
         model.getSnackbarMessage()
@@ -87,30 +106,49 @@ class ParentMapFragment : Fragment(), OnMapReadyCallback {
                 Snackbar.make(binding.container, it, Snackbar.LENGTH_SHORT)
                     .show()
             })
+        model.locationSettingsRequest()
+            .observe(this, Observer {
+                requestLocationSettings(it)
+            })
     }
 
-    private fun getLastLocation() {
-        viewModel.updateLastLocation()
-    }
-
-    private fun askForPermission() {
+    private fun askForLocationPermission() {
         //            if we need to show user additional information
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this.activity!!,
-                Manifest.permission.ACCESS_COARSE_LOCATION)) {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) ||
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
             val snackbar = Snackbar.make(binding.container, "Location permission needed to show location on map",
                 Snackbar.LENGTH_INDEFINITE)
             snackbar.setAction(android.R.string.ok) {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
                 snackbar.dismiss()
-                ActivityCompat.requestPermissions(this.activity!!, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    COARSE_LOCATION_REQUEST_CODE)
             }
             snackbar.show()
             Log.d(TAG, "showing extra information Location permission needed to show location on map")
         } else {
             Log.d(TAG, "no need to show anything, just show the dialog")
-            ActivityCompat.requestPermissions(this.activity!!, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                COARSE_LOCATION_REQUEST_CODE)
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
         }
+    }
+
+    private fun requestLocationSettings(builder: LocationSettingsRequest.Builder) {
+        LocationServices.getSettingsClient(this.activity!!)
+            .checkLocationSettings(builder.build())
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d(TAG, "settings are complete")
+                    viewModel.updateLocationRequest()
+                } else {
+                    val exception = it.exception
+                    Log.d(TAG, "exception occurred", exception)
+                    if (exception is ResolvableApiException) {
+                        Log.d(TAG, "exception resoultion starting", exception)
+                        startIntentSenderForResult(exception.resolution.intentSender, LOCATION_SETTINGS_REQUEST_CODE,
+                            Intent(), 0, 0, 0, Bundle())
+                    }
+                }
+            }
     }
 
     companion object {
--- Rent24Driver/app/src/main/java/com/rent24/driver/components/map/ParentMapViewModel.kt
+++ Rent24Driver/app/src/main/java/com/rent24/driver/components/map/ParentMapViewModel.kt
@@ -1,6 +1,7 @@
 package com.rent24.driver.components.map
 
 import android.Manifest
+import android.app.Activity
 import android.app.Application
 import android.content.pm.PackageManager
 import android.location.Location
@@ -9,7 +10,11 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.CameraUpdate
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
@@ -29,11 +34,14 @@ class ParentMapViewModel(application: Application) : AndroidViewModel(applicatio
     private val lastLocationOnCompleteListener by lazy {
         OnCompleteListener<Location> { t ->
             if (t.isSuccessful) {
-                Log.d(TAG, "location detected ${t.result?.latitude} : ${t.result?.longitude}")
-                lastLocation.value = t.result ?: Location("")
-                val latLng = LatLng(lastLocation.value?.latitude ?: 0.0, lastLocation.value?.longitude ?: 0.0)
-                updateMarker(latLng)
-                updateCamera(latLng)
+                if (null != t.result) {
+                    Log.d(TAG, "location detected ${t.result?.latitude} : ${t.result?.longitude}")
+                    lastLocation.value = t.result ?: Location("")
+                    val latLng = LatLng(lastLocation.value?.latitude ?: 0.0, lastLocation.value?.longitude ?: 0.0)
+                    updateMarker(latLng)
+                    updateCamera(latLng)
+                }
+                checkLocationSettings(locationSettingsRequest)
             } else {
                 Log.e(TAG, "error occured in detecting location", t.exception)
                 snackbarMessage.value = "Could not detect location"
@@ -43,6 +51,29 @@ class ParentMapViewModel(application: Application) : AndroidViewModel(applicatio
     private val snackbarMessage by lazy { MutableLiveData<String>() }
     private var listenerAdded = false
     private val askForLocationPermission by lazy { MutableLiveData<Boolean>() }
+    private val locationSettingsRequest by lazy { MutableLiveData<LocationSettingsRequest.Builder>() }
+    private val locationRequest by lazy {
+        LocationRequest.create()
+            .apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+    }
+    private val locationCallback by lazy {
+        object: LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                p0 ?: return
+                lateinit var latLng: LatLng
+                for (location in p0.locations) {
+                    latLng = LatLng(location.latitude, location.longitude)
+                    updateMarker(latLng)
+                    updateCamera(latLng)
+                }
+            }
+        }
+    }
+    private var detectLocation = false
 
     fun getLastLocationMarker(): LiveData<MarkerOptions> = lastLocationMarker
 
@@ -54,42 +85,35 @@ class ParentMapViewModel(application: Application) : AndroidViewModel(applicatio
 
     fun askForLocationPermission(): LiveData<Boolean> = askForLocationPermission
 
-    fun onMapReady() {
-        if (ContextCompat.checkSelfPermission(getApplication<Application>().applicationContext,
-                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            val location = locationClient.lastLocation
-            if (location.isSuccessful) {
-                lastLocation.value = location.result
-                val latLng = LatLng(lastLocation.value
-                    ?.latitude ?: 0.0, lastLocation.value
-                    ?.longitude ?: 0.0)
-                updateMarker(latLng)
-                updateCamera(latLng)
-            }
-        }
-    }
-
     fun updateLastLocation() {
-        //        check for permission
-        if (ContextCompat.checkSelfPermission(getApplication<Application>().applicationContext,
-                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if (!listenerAdded) {
-                // request already granted
-                locationClient
-                    .lastLocation
-                    .addOnCompleteListener(lastLocationOnCompleteListener)
-                listenerAdded = true
+        if (!detectLocation) {
+            detectLocation = true
+            //        check for permission
+            if (ContextCompat.checkSelfPermission(getApplication<Application>().applicationContext,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getApplication<Application>().applicationContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (!listenerAdded) {
+                    // request already granted
+                    locationClient
+                        .lastLocation
+                        .addOnCompleteListener(lastLocationOnCompleteListener)
+                    listenerAdded = true
+                }
+            } else {
+                listenerAdded = false
+                detectLocation = false
+                askForLocationPermission.value = true
+                askForLocationPermission.postValue(false)
             }
-        } else {
-            listenerAdded = false
-            askForLocationPermission.value = true
-            askForLocationPermission.postValue(false)
         }
     }
 
-    fun onRequestPermissionsResult(requestCode: Int, grantResults: IntArray, resultCode: Int) {
+    fun onRequestPermissionsResult(requestCode: Int, grantResults: IntArray, locationCode: Int) {
+        Log.d(TAG, "request code $requestCode")
         when (requestCode) {
-            resultCode -> {
+            locationCode -> {
+                Log.d(TAG, "checking result for coarse location")
                 if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     updateLastLocation()
                 } else {
@@ -99,12 +123,45 @@ class ParentMapViewModel(application: Application) : AndroidViewModel(applicatio
         }
     }
 
+    fun locationSettingsRequest(): LiveData<LocationSettingsRequest.Builder> = locationSettingsRequest
+
+    fun updateLocationRequest() {
+        if (ContextCompat.checkSelfPermission(getApplication<Application>().applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "fine location permission granted, updating location request")
+            locationClient.requestLocationUpdates(locationRequest,locationCallback, null)
+        } else {
+            Log.d(TAG, "permission denied, need to ask permission for fine location")
+        }
+    }
+
+    fun onActivityResult(requestCode: Int, resultCode: Int, expectedRequestCode: Int) {
+        when (requestCode) {
+            expectedRequestCode -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    updateLocationRequest()
+                } else {
+                    snackbarMessage.value = "Live location updates not allowed"
+                }
+            }
+        }
+    }
+
+    fun stopLocationUpdates() {
+        locationClient.removeLocationUpdates(locationCallback)
+    }
+
     private fun updateMarker(latLng: LatLng) {
         lastLocationMarker.value = MarkerOptions().position(latLng)
             .title("Last Location")
     }
 
     private fun updateCamera(latLng: LatLng) {
-        cameraUpdate.value = CameraUpdateFactory.newLatLng(latLng)
+        cameraUpdate.value = CameraUpdateFactory.newLatLngZoom(latLng, 18.0F)
+    }
+
+    private fun checkLocationSettings(locationSettingsRequestBuilder: MutableLiveData<LocationSettingsRequest.Builder>) {
+        locationSettingsRequestBuilder.value = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
     }
 }
